f97753f573a0fc4f62c284c9832a2974fe1c17c2
Anastasia Lucas <ani@theta.local>
2017-11-16 11:56:57 -0500

    server

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser,json());
+app.use(bodyParser.json());
+
+
+app.listen(PORT, function(){
+  console.log("App listening on port: " + PORT);
+
+});
